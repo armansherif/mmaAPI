f005323defaa1a838cb9c1e985deeb1a508e53ed
armansherif <armansh@hotmail.com>
2019-08-20 12:53:45 -0500

    creating repo and connecting

---
+++ mmaAPI/Models/Fighters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace mmaAPI.Models
+{
+    public class Fighters
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string FighterName { get; set; }
+
+        [BsonElement("win")]
+        public int Wins { get; set; }
+
+        [BsonElement("lose")]
+        public int Losses { get; set; }
+
+        public string Height { get; set; }
+
+        public string Weight { get; set; }
+
+        public string Reach { get; set; }
+    }
+
+}
